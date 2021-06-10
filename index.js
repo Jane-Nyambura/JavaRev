9e00f35b3d97f6efdc1b7d183f3cecc57f79d01b
Jane-Nyambura <79199503+Jane-Nyambura@users.noreply.github.com>
2021-06-10 20:05:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// function Intro() {
+//     var name = isName(document.getElementById('header').style);
+//     var cla = isCla(document.getElementById('header1').style);
+//     var subjects = isSubject(document.getElementById('header2').style);
+//     var date = isDate(document.getElementById('header3').style);
+//     console.log(name, cla, subjects, date)
+
+// }
+// Intro();function formValidation()
+function Quiz() {
+
+    var name = document.registration.username;
+    var add = document.registration.Subject;
+    var cla = document.registration.Class;
+    var name = document.registration.username;
+
+    if (allLetter(name)) {
+
+    }
+}
